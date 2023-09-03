d45cf7e00b17802a114704ed58d8366a1e209f3f
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-09-03 10:36:14 +0300

    add classwork

---
+++ Task002/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+**Задача 1:** Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от 1 до N.
+
+N = 5 -> "1, 2, 3, 4, 5"
+
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Answer(1, number));
+
+//Метод
+
+string Answer(int alpha, int numbers)
+{
+    if (alpha == numbers) return alpha.ToString();
+    return (alpha + " " + Answer(alpha+1, numbers));
+}
---
+++ Task004/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+**Задача 3:** Напишите программу, которая на вход принимает два числа A и B,
+ и возводит число А в целую степень B с помощью рекурсии.
+
+A = 3; B = 5 -> 243 (3⁵)
+
+A = 2; B = 3 -> 8
+*/
+
+Console.Write("Введите число для возведения в степень: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите степень, в которую надо возвести число: ");
+int range = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Число {number} в степени {range} равно: {Pow(number, range)}");
+
+int Pow(int number, int range)
+{
+    if (range == 0) return 1;
+    if (range == 1) return number;
+    return (number * Pow(number, range - 1));
+}
