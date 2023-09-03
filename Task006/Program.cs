0f23c72990ec52778458810ea14aa05bd16fc464
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-09-03 11:04:14 +0300

    add Task006 Sum of numbers between N and 1

---
+++ Task006/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+
+M = 4; N = 8. -> 30
+*/
+
+//Метод, который ищет сумму чисел в промежутке от M до N:
+
+int FindSumMN(int numM, int numN)
+{
+    if (numM == numN) return numN;
+    return (numM + FindSumMN(numM+1, numN));
+}
+
+// Сама программа:
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма натуральны чисел в промежутке от {m} до {n} равна: {FindSumMN(m,n)}");
